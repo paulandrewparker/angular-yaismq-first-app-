a9d397ca6252578232ad40bfa4c8974ed93ac729
paulandrewparker <paulandrewparker+1@gmail.com>
2020-03-24 15:29:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-yaismq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-yaismq)
---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'ebuPhone XL',
+    price: 1799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'ebuPhone Mini',
+    price: 1699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'ebuPhone Standard',
+    price: 1299,
+    description: ''
+  },
+  {
+    name: 'ebuPhoneX',
+    price: 1240,
+    description: 'The best option for those that need to navigate on ebu'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
